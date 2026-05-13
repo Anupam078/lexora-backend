813be0573c6ee9f6b7d8d6937640b7fe39aea51b
Anupam Ojha <anupamojha078@gmail.com>
2026-05-13 16:05:23 +0530

    Day 1 - Project bootstrap

---
+++ src/main/java/com/lexora/lexora_backend/LexoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lexora.lexora_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexoraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lexora/lexora_backend/LexoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexora.lexora_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
